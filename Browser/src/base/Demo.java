a6bbf823c3507d37e0360a5e164011da6266f653
hp <hp@DESKTOP-4U77Q68>
2020-12-29 11:32:32 +0530

    Mynewproject1

---
+++ Browser/src/base/Demo.java
@@ -0,0 +1,42 @@
+package base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Demo {
+	
+	
+
+	public static void main(String[] args) throws IOException {
+	File propFile=new File("config//BrowserDetails.properties");
+	Properties prop=new Properties();
+	FileInputStream fis=new FileInputStream(propFile);
+	prop.load(fis);
+	
+	
+	
+	String browserName=prop.getProperty("Browser") ;
+	WebDriver driver = null;
+	if(browserName.equals("Chrome")){
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+	}else if(browserName.equals("FireFox")){
+		System.setProperty("webdriver.gecko.driver", "drivers\\geckodriver.exe");
+		driver=new FirefoxDriver();
+	}else if(browserName.equals("IE")){
+		System.setProperty("webdriver.ie.driver", "drivers\\IEDriverServer.exe");
+		driver=new InternetExplorerDriver();
+	}
+	driver.get(prop.getProperty("URl"));
+
+	}
+}
+
+
